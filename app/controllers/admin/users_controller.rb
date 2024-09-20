897dc0dd0f6e85c39de03150843bd9872f44d97c
Niklas Trapp <niklas031712@gmail.com>
2024-09-20 15:36:50 +0200

    edit all users as admin functionality

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,50 @@
 class Admin::UsersController < ApplicationController
+    before_action :require_admin
+    before_action :set_user, only: [:edit, :update]
 
     def index
         @users = User.all
+    end
+
+    def destroy
+        @user = User.find(params[:id]) # Ensure the correct user is being found
+        if @user.destroy
+          flash[:notice] = "User deleted successfully"
+        else
+          flash[:alert] = "There was a problem deleting the user"
+        end
+        redirect_to admin_users_path
+      end
+      
+
+    def edit
+      @teams = Team.all # Assuming you have a Team model
+    end
+
+    def update
+      if @user.update(user_params)
+        flash[:notice] = "User updated successfully"
+        redirect_to admin_users_path
+      else
+        flash.now[:alert] = "There was a problem updating the user"
+        render :edit
+      end
+    end
+
+    private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:email, :name, :team_id, :role)
+    end
+
+    def require_admin
+      unless current_user&.role == 'admin'
+        flash[:alert] = "You are not authorized to access this section"
+        redirect_to root_path
+      end
     end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,19 @@ class ApplicationController < ActionController::Base
   # allow_browser versions: :modern
 
   helper_method :current_user
+  helper_method :is_admin
+  helper_method :is_manager
 
   def current_user
     @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
+  end
+
+  def is_admin
+    User.find_by(id: session[:user_id]).role == 'admin'
+  end
+
+  def is_manager
+    User.find_by(id: session[:user_id]).role == 'admin' || 'teamleader'
   end
 
   before_action :require_login
