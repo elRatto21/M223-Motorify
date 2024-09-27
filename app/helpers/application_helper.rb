cdae3cd126785b5560945ac9b2ebca6cb409349d
Niklas Trapp <niklas031712@gmail.com>
2024-09-27 14:03:07 +0200

    different styles for flash types

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
-end
+    def flash_class(type)
+      case type
+      when 'notice'
+        "flash-notice"  # for success messages
+      when 'alert'
+        "flash-alert"   # for error or warning messages
+      else
+        "flash-default" # for other types of messages
+      end
+    end
+  end
